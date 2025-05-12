0f60c42515ddc39e01048f7d432d674b97ce243f
vpat55 <vw0640800@gmail.com>
2025-05-12 09:13:49 +0530

    Time: 0 ms (100%), Space: 40.5 MB (50.58%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,9 +1,7 @@
 class Solution {
          int[] memo = new int[46];
-
     public int climbStairs(int n) {
          if(n <= 2) return n;
-
          if(memo[n] != 0) return memo[n];
 
          memo[n] = climbStairs(n-1) + climbStairs(n-2);
