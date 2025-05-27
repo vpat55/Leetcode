a4f5de5de11e1aa8b4e4ffa4fa484dab2452c7ef
vpat55 <vw0640800@gmail.com>
2025-05-27 09:20:42 +0530

    Time: 0 ms (100%), Space: 40.3 MB (83.37%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num < 2) return true;
+        int left = 2, right = num/2;
+
+        while(left <= right) {
+            int mid = left + (right - left)/2;
+            long decision = (long)mid * mid;
+
+            if(decision == num) return true;
+            else if(decision < num) left = mid + 1;
+            else right = mid - 1;
+        }
+        return false;
+    }
+}
